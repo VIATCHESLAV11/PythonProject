1a984e99d04f17bb104f9f82bc3934853b984bf5
Viatcheslav Shedov <slavashedov@yandex.ru>
2025-06-13 11:17:08 +0300

    final

--- src/widget.py
+++ src/widget.py
@@ -12,7 +12,6 @@ print(get_date("2024-03-11T02:26:18.671407"))
 # 11.03.2024  # выход функции
 
 
-
 def get_mask_card(card_number: str) -> str:
     """Маскировка номера карты"""
 
@@ -40,27 +39,21 @@ print(get_mask_account("73654108430135874305"))
 # **4305  # выход функции
 
 
-
-
-
-
-
-
-
 def mask_account_card(info_card: str) -> str:
     """Функция определяет счет это или номер карты и возвращает замаскированный"""
     info_card_list = info_card.split()
     if "Счет" in info_card_list:
         return f"Счет **{info_card_list[-1][-4:]}"
     else:
-        card_name = ' '.join(info_card_list[:-1])
-        card_number = info_card_list[-1].replace(' ', '')
+        card_name = " ".join(info_card_list[:-1])
+        card_number = info_card_list[-1].replace(" ", "")
         return f"{card_name} {get_mask_card(card_number)}"
 
+
 print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Visa Platinum 7000792289606361"))
+# Visa Platinum 7000792289606361  # входной аргумент
+# Visa Platinum 7000 79** **** 6361  # выход функции
 
-#Visa Platinum 7000792289606361  # входной аргумент
-#Visa Platinum 7000 79** **** 6361  # выход функции
-
-#Счет 73654108430135874305  # входной аргумент
-#Счет **4305  # выход функции
+# Счет 73654108430135874305  # входной аргумент
+# Счет **4305  # выход функции
