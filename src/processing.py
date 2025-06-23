82eb68d32d2ea9d733f8bc57bf8dab66b3a3cd6f
Viatcheslav Shedov <slavashedov@yandex.ru>
2025-06-23 17:41:40 +0300

    README.md updated with comments processing.py

--- src/processing.py
+++ src/processing.py
@@ -22,6 +22,7 @@ list_of_dict = [
 result = filter_by_state(list_of_dict)
 print(result)
 
+
 from datetime import datetime
 from typing import Dict, List, Union
 
@@ -31,6 +32,15 @@ operations_sort_by_date: List[Dict[str, Union[str, int]]] = [
     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
+
+# Входной аргумент
+# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+# Выход функции со статусом по умолчанию 'EXECUTED'
+# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+# Выход функции, если вторым аргументов передано 'CANCELED'
+# [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
 
 def sort_by_date(
@@ -59,3 +69,9 @@ print(
         ]
     )
 )
+
+# Входной аргумент
+# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+# Выход функции (сортировка по убыванию, т. е. сначала самые последние операции)
+# [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
